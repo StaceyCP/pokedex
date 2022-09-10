0380f2f4759cb0027b475bfca586f90525229388
Stacey <stacey6105@gmail.com>
2022-09-10 19:03:51 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [pokemon, setPokemon] = useState('');
+const [searchValue, setSearchValue] = useState('');
+const capitaliseFirstLetter = string => string[0].toUpperCase() + string.slice(1);
+
+
+const fetchPokemon = async () => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`);
+  if (response.status === 404) {
+    console.log("Pokemon not found")
+  } else if (!response.ok) {
+    console.log("Something went wrong. Please try again")
+  }
+  const data = await response.json();
+
+  setPokemon(data);
+}
+
+const handleInput = (e) => {
+  setSearchValue(e.target.value.toLowerCase());
+}
+
+const buttonClick = (e) => {
+  e.preventDefault();
+  fetchPokemon();
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Pokedex</h1>
+      {pokemon && 
+        <>
+          <img src={require(`./pokedexImg/${pokemon.name}.jpg`)} alt={`${pokemon.name} sprite`}></img>
+          <p>Pokemon name: {capitaliseFirstLetter(pokemon.name)}</p>
+          <p>Pokemon ID: {pokemon.id}</p> 
+        </>
+      }
+      <form className="searchField">
+        <input 
+          className="searchField__input"
+          onChange={handleInput} 
+          name='pokemon search'
+          placeholder='Search Pokemon Name'
+          value={searchValue}
         >
-          Learn React
-        </a>
-      </header>
+        </input>
+        <button onClick={buttonClick} className="searchField__button">Search</button>
+      </form>
     </div>
   );
 }
